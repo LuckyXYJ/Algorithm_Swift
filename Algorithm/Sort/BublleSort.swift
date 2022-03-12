587e7118b79cc8029bc8d9540400d4a35eed6fc3
邢雅杰 <470446087@qq.com>
2022-03-12 21:42:12 +0800

    BublleSort

--- Algorithm/Sort/BublleSort.swift
+++ Algorithm/Sort/BublleSort.swift
@@ -9,8 +9,7 @@ import Foundation
 
 class BublleSort: ArraySort {
     
-    static func sort(arr: [Int]) -> [Int] {
-        
+    static func sort1(arr: [Int]) -> [Int] {
         if arr.count <= 1 { return arr }
         var list = arr;
         for end in (0..<arr.count).reversed() {
@@ -22,4 +21,23 @@ class BublleSort: ArraySort {
         }
         return list
     }
+    
+    /// 已完成排序的提前终止排序
+    static func sort(arr: [Int]) -> [Int] {
+        if arr.count <= 1 { return arr }
+        var list = arr;
+        var sorted = true;
+        for end in (0..<arr.count).reversed() {
+            for i in 0..<end {
+                if list[i] > list[i + 1] {
+                    (list[i], list[i + 1]) = (list[i + 1], list[i])
+                    sorted = false
+                }
+            }
+            if sorted { break }
+        }
+        return list
+    }
+    
+    
 }
