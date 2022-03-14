c45e5790afe27c9665005a612e854cf8a1576c5e
邢雅杰 <470446087@qq.com>
2022-03-14 21:42:12 +0800

    BublleSort

--- Algorithm/Sort/BublleSort.swift
+++ Algorithm/Sort/BublleSort.swift
@@ -22,8 +22,8 @@ class BublleSort: ArraySort {
         return list
     }
     
-    /// 已完成排序的提前终止排序
-    static func sort(arr: [Int]) -> [Int] {
+    /// 优化 --- 已完成排序的提前终止排序
+    static func sort2(arr: [Int]) -> [Int] {
         if arr.count <= 1 { return arr }
         var list = arr;
         var sorted = true;
@@ -39,5 +39,30 @@ class BublleSort: ArraySort {
         return list
     }
     
+    /// 优化 --- 尾部有序部分位置记录，减少比较次数
+    static func sort(arr: [Int]) -> [Int] {
+        if arr.count <= 1 { return arr }
+        var list = arr;
+        recursion(list: &list, endIndex: arr.count)
+        return list
+    }
     
+    static func recursion(list: inout [Int], endIndex: Int) {
+        
+        if endIndex <= 1 {return}
+        
+        for end in (0..<endIndex).reversed() {
+            
+            var sortedIndex = 1;
+            for i in 0..<end {
+                if list[i] > list[i + 1] {
+                    (list[i], list[i + 1]) = (list[i + 1], list[i])
+                
+                    sortedIndex = i + 1;
+                }
+            }
+            self.recursion(list: &list, endIndex: sortedIndex)
+            break
+        }
+    }
 }
--- Algorithm/main.swift
+++ Algorithm/main.swift
@@ -9,8 +9,4 @@ import Foundation
 
 print("Hello, World!")
 
-var array: [Int] = [2,5,3,1,4,8,10,7,6,9,11,12,21,35,12,6]
-
-let arr = BublleSort.sort(arr: array);
-
-print(arr)
+SortTest.test()
