10eac52c8d8fb310cda2af24abf5d1fce83a96c3
邢雅杰 <470446087@qq.com>
2022-03-20 21:42:12 +0800

    InsertionSort

--- Algorithm/Sort/InsertionSort.swift
+++ Algorithm/Sort/InsertionSort.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /// 插入排序
-class InsertionSort: ArraySort {
+class InsertionSort1: ArraySort {
     
     static func sort(arr: [Int]) -> [Int] {
         if arr.count < 2 {return arr}
@@ -19,6 +19,25 @@ class InsertionSort: ArraySort {
                 list.swapAt(index, index - 1)
                 index -= 1
             }
+        }
+        return list
+    }
+}
+
+/// 插入排序优化1， 将逐个交换，转换为挪动
+class InsertionSort: ArraySort {
+    
+    static func sort(arr: [Int]) -> [Int] {
+        if arr.count < 2 {return arr}
+        var list = arr
+        for i in 1..<list.count {
+            var index = i
+            let temp = list[index];
+            while index > 0 && temp < list[index - 1]{
+                list[index] = list[index - 1]
+                index -= 1
+            }
+            list[index] = temp
         }
         return list
     }
