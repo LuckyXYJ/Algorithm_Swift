8ed2e97aa559b40b1711e50b9dc988f025454395
邢雅杰 <470446087@qq.com>
2022-04-01 21:42:12 +0800

    ListNode

---
+++ Algorithm/ListNode.swift
@@ -0,0 +1,31 @@
+//
+//  ListNode.swift
+//  Algorithm
+//
+//  Created by LuckyXYJ on 2023/3/10.
+//
+
+import Foundation
+
+public class ListNode {
+    
+    public var val: Int
+    
+    public var next: ListNode?
+    
+    public init() {
+        self.val = 0
+        self.next = nil
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+        
+    }
+    
+    public init(_ val: Int, _ next: ListNode?) {
+        self.val = val
+        self.next = next
+    }
+}
