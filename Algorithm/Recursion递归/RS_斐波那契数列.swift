2d67d95a6964e526e595ac737dfef9f5705cb3e1
邢雅杰 <470446087@qq.com>
2022-04-21 21:55:12 +0800

    斐波那契数列优化

--- Algorithm/Recursion递归/RS_斐波那契数列.swift
+++ Algorithm/Recursion递归/RS_斐波那契数列.swift
@@ -23,3 +23,31 @@ class fibRecursion {
 }
 
 
+/**
+    优化 - 记忆法
+    数组存放计算过的结果，避免重复计算，
+    时间复杂度降低O(n)，
+    空间复杂度O(n)
+ */
+class fibRecursion1 {
+    
+    static func fib(_ n: Int) -> Int {
+        
+        if n < 3 { return 1 }
+        
+        var arr = [Int].init(repeating: 0, count: n+1)
+        arr[1] = 1
+        arr[0] = 1
+        
+        return fibr(arr: &arr, n: n)
+    }
+    
+    static func fibr(arr: inout [Int], n: Int) -> Int {
+        if(arr[n] == 0) {
+            arr[n] = fibr(arr: &arr, n: n - 1) + fibr(arr: &arr, n: n - 2)
+        }
+        
+        return arr[n]
+    }
+}
+
