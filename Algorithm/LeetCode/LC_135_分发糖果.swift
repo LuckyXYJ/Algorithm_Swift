1f97a79708a6e7c3acc872addd706e165446ba64
邢雅杰 <470446087@qq.com>
2022-05-21 20:50:12 +0800

    分发糖果

---
+++ Algorithm/LeetCode/LC_135_分发糖果.swift
@@ -0,0 +1,30 @@
+//
+//  LC_135_分发糖果.swift
+//  Algorithm
+//
+//  Created by LuckyXYJ on 2023/3/10.
+//
+
+import Foundation
+
+class Solution {
+    func candy(_ ratings: [Int]) -> Int {
+        let n = ratings.count
+        var ans = Array(repeating: 1, count: n)
+        
+        for i in 1 ..< n {
+            if ratings[i] > ratings[i - 1] {
+                ans[i] = ans[i - 1] + 1
+            }
+        }
+        
+        for i in (0 ..< n - 1).reversed() {
+            if ratings[i] > ratings[i + 1] {
+                ans[i] = max(ans[i], ans[i + 1] + 1)
+            }
+        }
+        
+        return ans.reduce(0, +)
+    }
+}
+
