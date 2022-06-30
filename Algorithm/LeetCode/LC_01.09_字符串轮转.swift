45a886ce76f29d5fc5887c27a2d62c8d6d9c24bc
邢雅杰 <470446087@qq.com>
2022-06-30 20:50:12 +0800

    leetcode字符串轮转

---
+++ Algorithm/LeetCode/LC_01.09_字符串轮转.swift
@@ -0,0 +1,23 @@
+//
+//  LC_01.09_字符串轮转.swift
+//  Algorithm
+//
+//  Created by LuckyXYJ on 2023/3/11.
+//
+
+import Foundation
+
+class Solution {
+    func isFlipedString(_ s1: String, _ s2: String) -> Bool {
+        guard s1.count == s2.count else{
+            return false
+        }
+
+        if s1.isEmpty && s2.isEmpty {
+            return true
+    }
+        let newS = s2 + s2
+        
+        return newS.contains(s1)
+    }
+}
