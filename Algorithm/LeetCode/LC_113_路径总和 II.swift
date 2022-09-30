deab9459ea5e6a1ee53fc6cab21a3db5d9f14635
邢雅杰 <470446087@qq.com>
2022-09-30 20:50:12 +0800

    路径总和 II

--- Algorithm/LeetCode/LC_113_路径总和 II.swift
+++ Algorithm/LeetCode/LC_113_路径总和 II.swift
@@ -7,4 +7,26 @@
 
 import Foundation
 
-
+class Solution113 {
+    var res = [[Int]]()
+    func pathSum(_ root: TreeNode?, _ targetSum: Int) -> [[Int]] {
+      help(root, [], targetSum)
+      return res
+    }
+  
+    func help (_ root: TreeNode?, _ totalArr:[Int] , _ targetSum: Int) {
+      guard let root = root else {
+        return
+      }
+    
+      if root.left == nil && root.right == nil {
+        if targetSum == root.val {
+          res.append(totalArr + [root.val])
+        }
+        return
+      }
+    
+      help(root.left, totalArr + [root.val], targetSum - root.val)
+      help(root.right, totalArr + [root.val], targetSum - root.val)
+    }
+}
