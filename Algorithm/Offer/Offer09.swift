0088672badefb910c53dca91076baf62be4f5801
邢雅杰 <470446087@qq.com>
2023-01-18 20:33:12 +0800

    两个栈实现队列

--- Algorithm/Offer/Offer09.swift
+++ Algorithm/Offer/Offer09.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+
 class CQueue {
     
+    var st1:Stack<Int>
+    var st2:Stack<Int>
     
+    init() {
+        st1 = Stack<Int>()
+        st2 = Stack<Int>()
+    }
+    
+    func appendTail(_ value: Int) {
+        st1.push(value)
+    }
+    
+    func deleteHead() -> Int {
+        if st2.empty() {
+            while !st1.empty() {
+                st2.push(st1.pop()!)
+            }
+            return st2.pop() ?? -1
+        }else {
+            return st2.pop() ?? -1
+        }
+    }
 }
