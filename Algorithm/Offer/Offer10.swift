4c96cea9a33ab5549af65f9e94bc46f8fe4c8838
邢雅杰 <470446087@qq.com>
2023-01-27 20:33:12 +0800

    青蛙跳台阶

--- Algorithm/Offer/Offer10.swift
+++ Algorithm/Offer/Offer10.swift
@@ -8,7 +8,23 @@
 import Foundation
 
 class Offer10 {
+    
     func numWays(_ n: Int) -> Int {
+        
+        if n < 2 { return 1}
+        
+        var nums = [Int].init(repeating: 0, count: n + 1)
+        nums[0] = 1
+        nums[1] = 1
+        
+        for i in 2...n {
+            nums[i] = (nums[i-1] + nums[i-2])%1000000007
+        }
+        
+        return nums[i]
+    }
+    
+    func numWays1(_ n: Int) -> Int {
         if n == 1 || n == 0 { return 1 }
         
         return (numWays(n - 1) + numWays(n - 2))%1000000007
