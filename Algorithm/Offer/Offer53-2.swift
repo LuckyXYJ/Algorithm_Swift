51cc6df311a53c0f50ebefcab5bf0e53d960cfe4
邢雅杰 <470446087@qq.com>
2023-02-28 21:05:12 +0800

    offer53-2

--- Algorithm/Offer/Offer53-2.swift
+++ Algorithm/Offer/Offer53-2.swift
@@ -9,6 +9,13 @@ import Foundation
 
 class Solution {
     func missingNumber(_ nums: [Int]) -> Int {
-        
+        var res = 0
+        for i in 0..<nums.count {
+            if nums[i] != res {
+                return res
+            }
+            res += 1
+        }
+        return res
     }
 }
